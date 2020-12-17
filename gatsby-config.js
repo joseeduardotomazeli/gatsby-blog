cedf816811d2e894f9de93e50151ebf8d918fc2b
José Eduardo Piccoli Tomazeli <joseeduardo.tomazeli@outlook.com>
2020-12-17 02:17:08 -0300

    refactor: remove unused files

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Blog`,
+    description: `A blog about development.`,
+    author: `@gatsbyblog`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
