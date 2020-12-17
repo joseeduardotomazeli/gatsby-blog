384385c3202f853c860beed2479408e5cceb397e
José Eduardo Piccoli Tomazeli <joseeduardo.tomazeli@outlook.com>
2020-12-17 16:09:35 -0300

    feat: add profile component

---
+++ src/components/Profile/index.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+const Profile = () => (
+  <div className="Profile-wrapper">
+    <h1>José Eduardo Tomazeli</h1>
+    <h2>Front-end Developer</h2>
+    <p>Membro do Core Team da empresa X.</p>
+  </div>
+)
+
+export default Profile
