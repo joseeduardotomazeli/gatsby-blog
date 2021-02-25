7556cf9bb229b8c126ec8cae8b2854f9d3c96789
joseeduardotomazeli <joseeduardo.tomazeli@outlook.com>
2021-02-25 00:01:31 -0300

    feat: add layout to post

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,14 +1,19 @@
 import React from "react"
 import { graphql } from "gatsby"
 
-const Post = post => {
-  const { markdownRemark } = post.data
+import Layout from "../components/Layout"
+import SEO from "../components/seo"
+
+const Post = ({ data }) => {
+  const { markdownRemark } = data
 
   return (
-    <>
+    <Layout>
+      <SEO title={markdownRemark.frontmatter.title} />
+
       <h1>{markdownRemark.frontmatter.title}</h1>
       <div dangerouslySetInnerHTML={{ __html: markdownRemark.html }} />
-    </>
+    </Layout>
   )
 }
 
